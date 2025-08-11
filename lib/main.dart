145480df06c33f35119f29c0caa46a09e9f9c2da
Takeshi Yokemura <yokemura@ymck.net>
2025-08-11 13:47:40 +0900

    Removed dummy home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(
@@ -21,25 +22,6 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Custom Slide Show'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: const Center(
-        child: Text('Welcome to Custom Slide Show'),
-      ),
     );
   }
 }
